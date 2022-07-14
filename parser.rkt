5ce802311cb060f60e467e11ce364dd842622899
sepehrkianian091 <sepehrkianian09@gmail.com>
2022-07-14 14:03:08 +0430

    feat(parser): temp commit

--- parser.rkt
+++ parser.rkt
@@ -5,7 +5,7 @@
 
 (define python-parser
   (parser
-   (start exp)
+   (start Program)
    (end EOF)
    (error void)
    (tokens
@@ -16,7 +16,17 @@
     ;todo init_structure "ashk" || "sad"
     ;todo init_tree_creation "Team"
     (exp ((exp PLUS NUMBER) (list 'plusnumbers $1 $3))
-         ((NUMBER) (list 'anumber $1))))))
+         ((NUMBER) (list 'anumber $1)))
+    (Program ((Statements EOF) $1))
+    (Statements ((Statement SEMICOLON) (list $1))
+                ((Statements Statement SEMICOLON) (append $1 (list $2))))
+    (Statement ((Compound_stmt) $1)
+               ((Simple_stmt) $1))
+    (Simple_stmt ((Assignment) $1)
+                 ((Global_stmt) $1)
+                 ((Return_stmt) $1)
+                 (()))
+    )))
 
 ;test
 (define lex-this2 (lambda (lexer input) (lambda () (lexer input))))
