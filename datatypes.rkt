0d319588d4abaf13000ff8461fe9ccb76a201d22
sepehrkianian091 <sepehrkianian09@gmail.com>
2022-07-14 14:36:23 +0430

    feat(data-types): init

--- datatypes.rkt
+++ datatypes.rkt
@@ -1,5 +1,28 @@
 #lang racket
+(require (lib "eopl.ss" "eopl"))
+
+(define-datatype statement statement?
+  (assign (var symbol?) (expr expression?))
+  (global (var symbol?))
+  (return (expr expression?))
+  (return_void)
+  (pass)
+  (break)
+  (continue)
+  (func (name symbol?) (params list?) (statements list?))
+  (if_stmt (cond_exp expression?) (if_sts list?) (else_sts list?))
+  (for_stmt (iter symbol?) (list_exp expression?) (sts list?))
+  )
+
+(define-datatype func_params func_params?
+  ()
+  )
+
+(define-datatype expression expression?
+  ()
+  )
 
 ;todo init_structure "Team"
 
 (provide (all-defined-out))
+(#%provide (all-defined))
--- parser.rkt
+++ parser.rkt
@@ -3,6 +3,9 @@
 (require "lexer.rkt")
 (require parser-tools/yacc)
 
+(require "datatypes.rkt")
+(#%require "datatypes.rkt")
+
 (define python-parser
   (parser
    (start Program)
@@ -17,7 +20,7 @@
     ;todo init_tree_creation "Team"
     (exp ((exp PLUS NUMBER) (list 'plusnumbers $1 $3))
          ((NUMBER) (list 'anumber $1)))
-    (Program ((Statements EOF) $1))
+    (Program ((Statements) $1))
     (Statements ((Statement SEMICOLON) (list $1))
                 ((Statements Statement SEMICOLON) (append $1 (list $2))))
     (Statement ((Compound_stmt) $1)
@@ -25,7 +28,14 @@
     (Simple_stmt ((Assignment) $1)
                  ((Global_stmt) $1)
                  ((Return_stmt) $1)
-                 (()))
+                 ((PASS) #||#)
+                 ((BREAK) #||#)
+                 ((CONTINUE) #||#))
+    (Compound_stmt ((Function_def) $1)
+                   ((If_stmt) $1)
+                   ((For_stmt) $1)
+                   )
+    (Assignment (ID ASSIGN Expression) #||#)
     )))
 
 ;test
