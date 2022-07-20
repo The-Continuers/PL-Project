dc7045538d022c71e94e63cb7463d300ebe45490
ashkan <ashkan.khd.q@gmail.com>
2022-07-20 22:05:40 +0430

    feat: add exceptions

--- exceptions.rkt
+++ exceptions.rkt
@@ -1,15 +1,101 @@
 #lang racket
+(require (lib "eopl.ss" "eopl"))
+(require "./utils.rkt")
 
-(define (report-not-pair iter_list parent_stmt) #t)
+(define-datatype exception exception?
+    (new-exception (type symbol?) (message string?) (trace-back return-true))
+)
 
-(define (report-not-boolean cond-val parent_stmt) #t)
+(define (exception->exception-type e)
+    (cases exception e
+        (new-exception (type message trace-back) type)
+    )
+)
 
-(define (report-not-found scope var) #t)
+(define (exception->message e)
+    (cases exception e
+        (new-exception (type message trace-back) message)
+    )
+)
 
-(define (report-not-global -scope var) #t)
+(define (exception->trace-back e)
+    (cases exception e
+        (new-exception (type message trace-back) trace-back)
+    )
+)
 
-(define (report-break-outside-loop sts) #t)
+(define TypeError 'TypeError)
 
-(define (report-continue-outside-loop sts) #t)
+(define NameError 'NameError)
+
+(define SyntaxError 'SyntaxError)
+
+
+(define (raise e)
+    (eopl:error 
+        (exception->exception-type e) 
+        (string-join (list (exception->trace-back e) (exception->message e)) "\n")
+    )
+)
+
+(define (report-not-pair iter_list trace-back)
+    (raise 
+        (new-exception 
+        TypeError 
+        (string-join (list (~v iter_list) "object is not iterable" )) 
+        trace-back
+        )
+    )
+)
+
+(define (report-not-boolean cond-val trace-back) 
+    (raise 
+        (new-exception 
+        TypeError 
+        (string-join (list (~v cond-val) "object is not a bool type") " ") 
+        trace-back
+        )
+    )
+)
+
+(define (report-not-found trace-back var) 
+    (raise 
+        (new-exception 
+        NameError 
+        (string-join (list "name" (~v var) "is not defined" ) " ") 
+        trace-back
+        )
+    )
+)
+
+(define (report-not-global trace-back var)
+    (raise 
+        (new-exception 
+        SyntaxError 
+        (string-join (list "name" (~v var) "is assigned to before global declaration" ) " ") 
+        trace-back
+        )
+    )
+)
+
+(define (report-break-outside-loop trace-back)
+    (raise 
+        (new-exception 
+        SyntaxError 
+        "'break' outside loop" 
+        trace-back
+        )
+    )
+)
+
+(define (report-continue-outside-loop trace-back) 
+    (raise 
+        (new-exception 
+        SyntaxError 
+        "'break' outside loop" 
+        trace-back
+        )
+    )
+)
 
 (provide (all-defined-out))
