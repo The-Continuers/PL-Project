b695b4d5983838621c127d389317c57f1a99490a
sepehrkianian091 <sepehrkianian09@gmail.com>
2022-07-21 01:34:39 +0430

    test(execution): init function_with_params definition, call tests

---
+++ tests/set_up_program.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require "../datatypes.rkt")
+
+(define program-test-1 (list (func "fi" (func_params (with_default "x" (atomic_num_exp 0)))
+                                   (list (if_stmt
+                                          (binary_op (lambda (x y) (or x y))
+                                                     (binary_op equal? (ref "x") (atomic_num_exp 0))
+                                                     (binary_op equal? (ref "x") (atomic_num_exp 1))
+                                                     )
+                                          (return (atomic_num_exp 1))
+                                          (return (binary_op +
+                                                             (function_call (ref "fi")
+                                                                            (expressions
+                                                                             (binary_op - (ref "x") (atomic_num_exp 1))
+                                                                             (empty-expr))
+                                                                            (expressions
+                                                                             (binary_op - (ref "x") (atomic_num_exp 2))
+                                                                             (empty-expr))
+                                                                            ))))))
+                             (return (function_call (ref "fi")
+                                                    (expressions (atomic_num_exp 5) (empty-expr))))
+                             ))
+
+(define program-test-2 (list (func "gi" (func_params (with_default "x" (atomic_num_exp 0))
+                                                     (func_params (with_default "y" (atomic_num_exp 1)) (empty-param)))
+                                   (list (return (binary_op / (ref "x") (ref "y")))))
+                             (return (function_call (ref "gi")
+                                                    (expressions (atomic_num_exp 5)
+                                                                 (expressions (atomic_num_exp 2) (empty-expr)))))
+                             ))
+
+
+
+(provide (all-defined-out))
