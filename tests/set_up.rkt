f5a57993ea377c060a297d7c95b017f0311e93e9
sepehrkianian091 <sepehrkianian09@gmail.com>
2022-07-21 00:16:58 +0430

    feat(test): add function to scope

--- tests/set_up.rkt
+++ tests/set_up.rkt
@@ -6,9 +6,23 @@
 (require "../interpreter/scope/env.rkt")
 (#%require "../interpreter/scope/env.rkt")
 
+(require "../interpreter/interpreter_data.rkt")
+(#%require "../interpreter/interpreter_data.rkt")
+
+(require "../datatypes.rkt")
+(#%require "../datatypes.rkt")
+
 (reset-scope)
 (add-scope (init-scope))
 (extend-scope-index ROOT "y" 2)
 (extend-scope-index ROOT "l" (list 10 12 1 3 16))
+(extend-scope-index ROOT "f" (new-proc (empty-eval-func-param)
+                                       (list (assign "x" (atomic_num_exp 3))
+                                             (func "g" (empty-param) (list
+                                                                      (return (binary_op * (ref "x") (atomic_num_exp 2)))
+                                                                      ))
+                                             (assign "x" (atomic_num_exp 4))
+                                             (return (function_call (ref "g") (empty-expr)))
+                                             )))
 (add-scope (new-scope (extend-env "y" 3 (init-env)) ROOT (list)))
 (add-scope (new-scope (extend-env "x" 12 (init-env)) 1 (list "y")))
